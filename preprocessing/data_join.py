e3bcdffb0706017c3f1d396a55024d3e1d233447
ishank09 <ishankvasania09@gmail.com>
2023-02-25 23:42:58 -0800

    changed logic for joining table to get more accurate result without dublicate rows

--- preprocessing/data_join.py
+++ preprocessing/data_join.py
@@ -16,14 +16,51 @@ def read_data():
     raise FileExistsError: raises an exception when file is not found
     """
     try:
-        fraud=pd.read_csv("../data/Train-1542865627584.csv")
-        beneficiary=pd.read_csv("../data/Train_Beneficiarydata-1542865627584.csv")
-        inpatient=pd.read_csv("../data/Train_Inpatientdata-1542865627584.csv")
-        outpatient=pd.read_csv("../data/Train_Outpatientdata-1542865627584.csv")
+        fraud=pd.read_csv("data/Train-1542865627584.csv")
+        beneficiary=pd.read_csv("data/Train_Beneficiarydata-1542865627584.csv")
+        inpatient=pd.read_csv("data/Train_Inpatientdata-1542865627584.csv")
+        outpatient=pd.read_csv("data/Train_Outpatientdata-1542865627584.csv")
+        return fraud, beneficiary, inpatient, outpatient
     except FileExistsError as error:
         raise error
-    return fraud, beneficiary, inpatient, outpatient
     
+
+def join_inpatient_outpatient(inpatient, outpatient):
+    inpatient_outpatient=pd.merge(outpatient,inpatient,
+                                left_on=['BeneID', 'ClaimID', 'ClaimStartDt', 'ClaimEndDt', 'Provider',
+        'InscClaimAmtReimbursed', 'AttendingPhysician', 'OperatingPhysician',
+        'OtherPhysician', 'ClmDiagnosisCode_1', 'ClmDiagnosisCode_2',
+        'ClmDiagnosisCode_3', 'ClmDiagnosisCode_4', 'ClmDiagnosisCode_5',
+        'ClmDiagnosisCode_6', 'ClmDiagnosisCode_7', 'ClmDiagnosisCode_8',
+        'ClmDiagnosisCode_9', 'ClmDiagnosisCode_10', 'ClmProcedureCode_1',
+        'ClmProcedureCode_2', 'ClmProcedureCode_3', 'ClmProcedureCode_4',
+        'ClmProcedureCode_5', 'ClmProcedureCode_6', 'DeductibleAmtPaid',
+        'ClmAdmitDiagnosisCode'],
+                                right_on=['BeneID', 'ClaimID', 'ClaimStartDt', 'ClaimEndDt', 'Provider',
+        'InscClaimAmtReimbursed', 'AttendingPhysician', 'OperatingPhysician',
+        'OtherPhysician', 'ClmDiagnosisCode_1', 'ClmDiagnosisCode_2',
+        'ClmDiagnosisCode_3', 'ClmDiagnosisCode_4', 'ClmDiagnosisCode_5',
+        'ClmDiagnosisCode_6', 'ClmDiagnosisCode_7', 'ClmDiagnosisCode_8',
+        'ClmDiagnosisCode_9', 'ClmDiagnosisCode_10', 'ClmProcedureCode_1',
+        'ClmProcedureCode_2', 'ClmProcedureCode_3', 'ClmProcedureCode_4',
+        'ClmProcedureCode_5', 'ClmProcedureCode_6', 'DeductibleAmtPaid',
+        'ClmAdmitDiagnosisCode']
+                                ,how='outer')
+    return inpatient_outpatient
+
+def join_inpatient_outpatient_beneficiary(inpatient_outpatient, beneficiary):
+    inpatient_outpatient_beneficiary=pd.merge(inpatient_outpatient,beneficiary,left_on='BeneID',right_on='BeneID',how='inner')
+    return inpatient_outpatient_beneficiary
+
+
+
+def join_inpatient_outpatient_beneficiary_fraud(inpatient_outpatient_beneficiary, fraud):
+    inpatient_outpatient_beneficiary_fraud=pd.merge(fraud,inpatient_outpatient_beneficiary,on='Provider')
+    return inpatient_outpatient_beneficiary_fraud
+
+
+
+
 def join_csv(fraud, beneficiary, inpatient, outpatient):
     """
     function to join csv files. Joining by key BeneID, Provider.
@@ -31,12 +68,12 @@ def join_csv(fraud, beneficiary, inpatient, outpatient):
     return: merged csv
     """
     # Join the files
-    merged = pd.merge(beneficiary, inpatient, on='BeneID', how='outer')
-    merged = pd.merge(merged, outpatient, on='BeneID', how='outer')
-    merged = pd.merge(merged, train, on='Provider', how='left')
+    merged = join_inpatient_outpatient(inpatient, outpatient)
+    merged = join_inpatient_outpatient_beneficiary(merged, beneficiary)
+    merged = join_inpatient_outpatient_beneficiary_fraud(merged, fraud)
 
     # Save the merged file as a CSV
-    merged.to_csv('../data/merged.csv', index=False)
+    merged.to_csv('data/merged.csv', index=False)
 
 def generate_merged_data():
     """
