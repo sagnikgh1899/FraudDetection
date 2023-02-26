cdb307e623b9cd02eeed490abf299e6ca1eaa5f4
ishank09 <ishankvasania09@gmail.com>
2023-02-25 22:41:52 -0800

    adding initial eda func

--- preprocessing/initial_eda.py
+++ preprocessing/initial_eda.py
@@ -4,14 +4,36 @@ imports: numpy, pandas
 """
 import pandas as pd
 
-def get_dimention():
+def read_data():
     """
+    function to read csv file
+    parameters: None
+    return: data frames fraud, beneficiary, inpatient, outpatient.
+    raise FileExistsError: raises an exception when file is not found
     """
-    print('Train Shape:',Train.shape,'\n')
-    print('Train Sample:\n',Train.head(2),'\n')
+    try:
+        merged=pd.read_csv("../data/merged.csv")
+    except FileExistsError as error:
+        raise error
+    return merged
 
-    print('\n Test Shape:',Test.shape,'\n')
-    print('Test Sample: \n',Test.head(2))
-    print('\n Total missing values in Train :',Train.isna().sum().sum())
+def explore_data(dataframe):
+    """
+    function to get number of rows and columns, all columns, to show first 5 data.
+    parameters: merged dataset
+    return: None    
+    """
+    print("Number of rows and columns:", dataframe.shape)
+    print("\nColumn names:\n", dataframe.columns)
+    print("\nFirst 5 rows:\n", dataframe.head())
+def missing_data(dataframe):
+    """
+    function to prints the percentage of missing values in each column.
+    parameters: merged dataset
+    return: None
+    """
+    total_missing = dataframe.isnull().sum().sort_values(ascending=False)
+    percent_missing = (total_missing / len(dataframe)) * 100
+    missing_data = pd.concat([total_missing, percent_missing], axis=1, keys=['Total', 'Percent'])
+    print("Percentage of missing values in each column:\n", missing_data)
 
-    print('\n Total missing values in Train :',Test.isna().sum().sum())
