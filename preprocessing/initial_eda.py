514d9dcdbfa1d1cb45021c0e47c629cd4076b0ff
ishank09 <ishankvasania09@gmail.com>
2023-02-25 23:08:46 -0800

    adding boxplot, heatmap etc

--- preprocessing/initial_eda.py
+++ preprocessing/initial_eda.py
@@ -3,6 +3,9 @@ Module to perform initial eda on merged dataset from data_join.py to get initial
 imports: numpy, pandas
 """
 import pandas as pd
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def read_data():
     """
@@ -17,7 +20,7 @@ def read_data():
         raise error
     return merged
 
-def unique_values(dataframe):
+def get_unique_values(dataframe):
     """
     function to get unique values of dataframe
     parameters: merged dataset
@@ -27,17 +30,18 @@ def unique_values(dataframe):
     print("Number of unique values:\n", unique_vals)
 
 
-def explore_data(dataframe):
+def get_dimention(dataframe):
     """
     function to get number of rows and columns, all columns, to show first 5 data.
     parameters: merged dataset
     return: None    
     """
+    print(dataframe.describe())
     print("Number of rows and columns:", dataframe.shape)
     print("\nColumn names:\n", dataframe.columns)
     print("\nFirst  rows:\n", dataframe.head())
 
-def missing_data(dataframe):
+def get_missing_data(dataframe):
     """
     function to prints the percentage of missing values in each column.
     parameters: merged dataset
@@ -48,3 +52,36 @@ def missing_data(dataframe):
     missing_data = pd.concat([total_missing, percent_missing], axis=1, keys=['Total', 'Percent'])
     print("Percentage of missing values:\n", missing_data)
 
+
+def plot_heatmap(data):
+    """
+    function to plot heat map 
+    parameters: merged dataset
+    return: None
+    """
+    corr = data.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+def plot_boxplots(data, columns):
+    """
+    function to plot boxplots
+    parameters: merged dataset, columns for which we want to plot 
+    return: None
+    """
+    for column in columns:
+        plt.figure(figsize=(8, 6))
+        sns.boxplot(x='PotentialFraud', y=column, data=data)
+        plt.title(f"{column} by PotentialFraud")
+        plt.show()
+
+def get_correlation(data):
+    """
+    function get correlation
+    parameters: merged dataset
+    return: top 5 columns having highest correlation with PotentialFraud
+    """
+    corr_to_target = data.corr()['PotentialFraud'].abs().sort_values(ascending=False)
+    return corr_to_target[1:6].index
