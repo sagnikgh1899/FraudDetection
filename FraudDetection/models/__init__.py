3695eebc9ee6b025d676f07542e60b371c83f284
Sagnik Ghosal <sagnikghosal1999@gmail.com>
2023-03-11 12:39:44 -0800

    Updated models.py file

--- FraudDetection/models/__init__.py
+++ FraudDetection/models/__init__.py
@@ -1,15 +1,15 @@
 """
 Implements the models for fraud detection
 Functions:
-    lof_anomaly_detection: Uses the LOF model
-    knn_anomaly_detection: Uses the KNN model
     copod_anomaly_detection: Uses the COPOD model
-    abod_anomaly_detection: Uses the ABOD model
     ecod_anomaly_detection: Uses the ECOD model
+    iforest_anomaly_detection: Uses the ECOD model
+    loda_anomaly_detection: Uses the ECOD model
+    suod_anomaly_detection: Uses the ECOD model
 """
 # pylint: disable=E0401
-from .models import lof_anomaly_detection
-from .models import knn_anomaly_detection
-from .models import copod_anomaly_detection
-from .models import abod_anomaly_detection
+from .models import loda_anomaly_detection
 from .models import ecod_anomaly_detection
+from .models import copod_anomaly_detection
+from .models import iforest_anomaly_detection
+from .models import suod_anomaly_detection
