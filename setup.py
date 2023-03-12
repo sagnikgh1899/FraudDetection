f6cf9e3080b171c76cfad70ce8018d1a05ddca9a
SAGNIK GHOSAL <sagnikghosal1999@gmail.com>
2023-03-11 17:42:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,20 @@ from setuptools import setup, find_packages
 
 PACKAGES = find_packages()
 
-opts = dict(name='FraudDetection',
-            maintainer='Sagnik Ghosal, Ishank Vasania, Prerit Chaudhary, Neel Shah',
-            maintainer_email='fraud.detect@gmail.com',
-            description='A Fraud Detection Tool Designed to Analyze Medical Claims',
-            long_description='This tool is a fraud detection system designed to analyze medical insurance claims to '
-                             'identify potential fraudulent activities.',
-            url='https://github.com/sagnikgh1899/FraudDetection',
-            # download_url="",
-            license='MIT',
-            # classifiers="CLASSIFIERS",
-            author='Sagnik Ghosal, Ishank Vasania, Prerit Chaudhary, Neel Shah',
-            author_email='fraud.detect@gmail.com',
-            version='1.2',
-            packages=['pandas', 'numpy', 'pyod', 'suod', 'bokeh', 'flask', 'plotly', 'seaborn', 'matplotlib'],
-            # package_data="None",
-            # install_requires=["python3", "jupyter notebook"],
-            requires=['pandas', 'numpy', 'pyod', 'suod', 'bokeh', 'flask', 'plotly', 'seaborn', 'matplotlib'])
+opts = dict(
+    name='FraudDetection',
+    maintainer='Sagnik Ghosal, Ishank Vasania, Prerit Chaudhary, Neel Shah',
+    maintainer_email='fraud.detect@gmail.com',
+    description='A Fraud Detection Tool Designed to Analyze Medical Claims',
+    long_description='This tool is a fraud detection system designed to analyze medical insurance claims to identify potential fraudulent activities.',
+    url='https://github.com/sagnikgh1899/FraudDetection',
+    license='MIT',
+    author='Sagnik Ghosal, Ishank Vasania, Prerit Chaudhary, Neel Shah',
+    author_email='fraud.detect@gmail.com',
+    version='1.2',
+    install_requires=['pandas', 'numpy', 'pyod', 'suod', 'bokeh', 'flask', 'plotly', 'seaborn', 'matplotlib'],
+    packages=PACKAGES,
+)
 
 if __name__ == '__main__':
     setup(**opts)
