a36c561f0e807e906438f31c315e9316cf9beda1
Sagnik Ghosal <sagnikghosal1999@gmail.com>
2023-03-11 22:30:57 -0800

    Moved preprocessing folder inside FraudDetection

---
+++ FraudDetection/preprocessing/initial_eda.py
@@ -0,0 +1,134 @@
+"""
+Module to perform initial eda on merged dataset from data_join.py to get
+initial idea about the dataset for preprocessing and further computations.
+imports: numpy, pandas
+"""
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def read_data():
+    """
+    function to read csv file
+    parameters: None
+    return: data frames fraud, beneficiary, inpatient, outpatient.
+    raise FileExistsError: raises an exception when file is not found
+    """
+    try:
+        merged=pd.read_csv("./FraudDetection/data/preprocessed.csv")
+    except FileExistsError as error:
+        raise error
+    return merged
+
+
+def get_unique_values(dataframe):
+    """
+    function to get unique values of dataframe
+    parameters: merged dataset
+    return: None
+    """
+    unique_vals = dataframe.nunique().sort_values()
+    print("Number of unique values:\n", unique_vals)
+
+
+def get_dimention(dataframe):
+    """
+    function to get number of rows and columns, all columns, to show first 5 data.
+    parameters: merged dataset
+    return: None
+    """
+    print(dataframe.describe())
+    print("Number of rows and columns:", dataframe.shape)
+    print("\nColumn names:\n", dataframe.columns)
+    print("\nFirst  rows:\n", dataframe.head())
+
+
+def get_missing_data(dataframe):
+    """
+    function to prints the percentage of missing values in each column.
+    parameters: merged dataset
+    return: None
+    """
+    total_missing = dataframe.isnull().sum().sort_values(ascending=False)
+    percent_missing = (total_missing / len(dataframe)) * 100
+    missing_data = pd.concat([total_missing, percent_missing], axis=1, keys=['Total', 'Percent'])
+    print("Percentage of missing values:\n", missing_data)
+
+
+def plot_heatmap(dataframe):
+    """
+    function to plot heat map
+    parameters: merged dataset
+    return: None
+    """
+    corr = dataframe.corr()
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+
+def plot_boxplots(dataframe, columns):
+    """
+    function to plot boxplots
+    parameters: merged dataset, columns for which we want to plot
+    return: None
+    """
+    for column in columns:
+        plt.figure(figsize=(8, 6))
+        sns.boxplot(x='PotentialFraud', y=column, data=dataframe)
+        plt.title(f"{column} by PotentialFraud")
+        plt.show()
+
+
+def get_admit_analysis(dataframe):
+    """
+    function to get range of admitdays
+    parameters: merged dataset, columns for which we want to plot
+    return: None
+    """
+    # Lets check Min and Max values of AdmitforDays column in Train and Test.
+    print('Min Admit:- ',dataframe.AdmitForDays.min())
+    print('Max Admit:- ',dataframe.AdmitForDays.max())
+    dataframe.AdmitForDays.isnull().sum()   #Check Null values.
+
+
+def get_correlation(dataframe):
+    """
+    function get correlation
+    parameters: merged dataset
+    return: top 5 columns having highest correlation with PotentialFraud
+    """
+    corr_to_target = dataframe.corr()['PotentialFraud'].abs().sort_values(ascending=False)
+    return corr_to_target[1:6].index
+
+
+def get_eda():
+    """
+    function get complete eda
+    parameters: None
+    return: None
+    """
+    merged = read_data()
+    saved = merged
+    get_unique_values(merged)
+    get_dimention(merged)
+    get_missing_data(merged)
+    get_admit_analysis(merged)
+    plot_heatmap(merged)
+    top_5_corr = get_correlation(merged)
+    plot_boxplots(saved, top_5_corr)
+
+
+def main():
+    """
+    main function
+    parameters: None
+    return: None
+    """
+    get_eda()
+
+
+if __name__ == "__main__":
+    main()
---
+++ FraudDetection/preprocessing/preprocessing.py
@@ -0,0 +1,209 @@
+"""
+Module to perform data preprocesssing. This will be first level preprocessing. For visualization
+and machine learning modeling seperate preprocessing will be required according to the requirements.
+
+imports: pandas
+"""
+
+# pylint: disable=W0612:
+from datetime import datetime
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def read_data():
+    """
+    function to read csv file
+    parameters: None
+    return: data frames fraud, beneficiary, inpatient, outpatient.
+    raise FileExistsError: raises an exception when file is not found
+    """
+    try:
+        merged = pd.read_csv("./FraudDetection/data/merged.csv")
+    except FileExistsError as error:
+        raise error
+    return merged
+
+
+def encoding_potential_fraud(dataframe):
+    """
+    function encode catagorical field having yes/no into a numerical field
+    parameters: merged dataset
+    return: modified dataframe with potencialfraud encoded into numerical value
+    """
+    dataframe['PotentialFraud'] = dataframe['PotentialFraud'].map({'Yes': 1, 'No': 0})
+    # corr_to_target = dataframe.corr()['PotentialFraud'].abs().sort_values(ascending=False)
+    return dataframe
+
+
+def encoding_catagorical_data(dataframe):
+    """
+    function encode catagorical field having yes/no into a numerical field
+    parameters: merged dataset
+    return: modified dataframe with catagorical columns encoded into numerical value
+    """
+    dataframe = dataframe.replace({'ChronicCond_Alzheimer': 2,
+                                   'ChronicCond_Heartfailure': 2,
+                                   'ChronicCond_KidneyDisease': 2,
+                                   'ChronicCond_Cancer': 2,
+                                   'ChronicCond_ObstrPulmonary': 2,
+                                   'ChronicCond_Depression': 2,
+                                   'ChronicCond_Diabetes': 2,
+                                   'ChronicCond_IschemicHeart': 2,
+                                   'ChronicCond_Osteoporasis': 2,
+                                   'ChronicCond_rheumatoidarthritis': 2,
+                                   'ChronicCond_stroke': 2}, 0)
+
+    dataframe = dataframe.replace({'RenalDiseaseIndicator': 'Y'}, 1)
+
+    dataframe = dataframe.replace(
+        {'ChronicCond_Alzheimer': 2, 'ChronicCond_Heartfailure': 2, 'ChronicCond_KidneyDisease': 2,
+         'ChronicCond_Cancer': 2, 'ChronicCond_ObstrPulmonary': 2, 'ChronicCond_Depression': 2,
+         'ChronicCond_Diabetes': 2, 'ChronicCond_IschemicHeart': 2, 'ChronicCond_Osteoporasis': 2,
+         'ChronicCond_rheumatoidarthritis': 2, 'ChronicCond_stroke': 2}, 0)
+
+    dataframe = dataframe.replace({'RenalDiseaseIndicator': 'Y'}, 1)
+
+    return dataframe
+
+
+def add_admit_column(dataframe):
+    """
+    function to add a column admitdays
+    parameters: merged dataset
+    return: modified dataframe with added admitfordays column
+    """
+    dataframe['AdmissionDt'] = pd.to_datetime(dataframe['AdmissionDt'], format='%Y-%m-%d')
+    dataframe['DischargeDt'] = pd.to_datetime(dataframe['DischargeDt'], format='%Y-%m-%d')
+    dataframe['AdmitForDays'] = ((dataframe['DischargeDt'] - dataframe['AdmissionDt']).dt.days) + 1
+    return dataframe
+
+
+def add_age_column(dataframe):
+    """
+    function to add a column age based on DOB and DOD
+    parameters: merged dataset
+    return: modified dataframe with added age column
+    """
+    dataframe['DOB'] = pd.to_datetime(dataframe['DOB'], format='%Y-%m-%d')
+    dataframe['DOD'] = pd.to_datetime(dataframe['DOD'], format='%Y-%m-%d', errors='ignore')
+    dataframe['Age'] = round(((dataframe['DOD'] - dataframe['DOB']).dt.days) / 365)
+    dataframe.Age.fillna(round(((pd.to_datetime('2009-12-01', format='%Y-%m-%d') -
+                                 dataframe['DOB']).dt.days) / 365),
+                         inplace=True)
+
+    dataframe.Age.fillna(round(((pd.to_datetime('2009-12-01', format='%Y-%m-%d') -
+                                 dataframe['DOB']).dt.days) / 365),
+                         inplace=True)
+    return dataframe
+
+
+def adding_dead_column(dataframe):
+    """
+    function to add a column to get if the person is dead or not
+    parameters: merged dataset
+    return: modified dataframe with added WhetherDead column
+    """
+    dataframe.loc[dataframe.DOD.isna(), 'WhetherDead'] = 0
+    dataframe.loc[dataframe.DOD.notna(), 'WhetherDead'] = 1
+    dataframe.loc[:, 'WhetherDead'].head(7)
+    return dataframe
+
+
+def create_columns_visualization(dataframe,state_mapping):
+    """
+    function to add a column to get if the person is dead or not
+    parameters: merged dataset
+    return: modified dataframe with added WhetherDead column
+    """
+    inpatient_final_df = dataframe.loc[dataframe['is_Inpatient'] ==1]
+    #inpatient_final_df['DischargeDt'] = inpatient_final_df['DischargeDt'].apply(
+    #    lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    #inpatient_final_df['AdmissionDt'] = inpatient_final_df['AdmissionDt'].apply(
+    #    lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['ClaimStartDt'] = inpatient_final_df['ClaimStartDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['ClaimEndDt'] = inpatient_final_df['ClaimEndDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    #inpatient_final_df['Day_admitted'] = (inpatient_final_df['DischargeDt']-
+    #inpatient_final_df['AdmissionDt'])
+    #inpatient_final_df['Day_admitted'] = inpatient_final_df['Day_admitted'].apply(
+    #    lambda x: x.days)
+    inpatient_final_df['Age'] = inpatient_final_df['DOB'].apply(
+     lambda x: datetime.strptime("2013-03-03", '%Y-%m-%d').year - x.year)
+    inpatient_final_df = inpatient_final_df.merge(state_mapping, on ='State', how = 'left')
+    inpatient_final_df.loc[(inpatient_final_df['AdmitForDays'] <= 20),
+         "Days_Admitted_Bucket"] = "0-20 Days"
+    inpatient_final_df.loc[((inpatient_final_df['AdmitForDays'] > 20)),
+         "Days_Admitted_Bucket"] = "More than 20 Days"
+    inpatient_final_df.loc[inpatient_final_df['InscClaimAmtReimbursed']<=20000,
+         'InscClaimAmtReimbursed_Bucket'] = '0 - 20000'
+    inpatient_final_df.loc[((inpatient_final_df['InscClaimAmtReimbursed'] > 20000) &
+         (inpatient_final_df['InscClaimAmtReimbursed']<=40000)),
+         'InscClaimAmtReimbursed_Bucket'] = '20000 - 40000'
+    inpatient_final_df.loc[((inpatient_final_df['InscClaimAmtReimbursed'] > 40000)
+         & (inpatient_final_df['InscClaimAmtReimbursed']<=60000)),
+         'InscClaimAmtReimbursed_Bucket'] = '40000 - 60000'
+    inpatient_final_df.loc[inpatient_final_df['InscClaimAmtReimbursed']> 60000,
+         'InscClaimAmtReimbursed_Bucket'] = 'Greater than 60000'
+    return inpatient_final_df
+
+
+def save_csv(dataframe):
+    """
+    function to save preprocessed data into a csv file, to be used for further
+    computations of the project.
+    parameters: merged dataset
+    return: None
+    """
+    dataframe.to_csv('./FraudDetection/data/preprocessed.csv', index=False)
+
+
+def save_test_data(x_test,y_test):
+    """
+    function to save test data into a csv file, to be used for further
+    computations of the project.
+    parameters: merged dataset
+    return: None
+    """
+    x_test.to_csv('./FraudDetection/data/test.csv', index=False)
+    y_test.to_csv('./FraudDetection/data/test_labels.csv', index=False)
+
+
+def pre_processing():
+    """
+    function to read and then preprocess data
+    parameters: merged dataset
+    return: modified dataframe with added WhetherDead column
+    """
+    dataframe = read_data()
+    dataframe = encoding_catagorical_data(dataframe)
+    dataframe = add_admit_column(dataframe)
+    dataframe = add_age_column(dataframe)
+    dataframe = adding_dead_column(dataframe)
+    dataframe = encoding_potential_fraud(dataframe)
+    return dataframe
+
+
+def main():
+    """
+    main function
+    parameters: None
+    return: None
+    """
+    dataframe = pre_processing()
+    save_csv(dataframe)
+    state_mapping = pd.read_csv("./FraudDetection/data/State_Mapping.csv")
+    visualization_dataframe = create_columns_visualization(dataframe,state_mapping)
+    visualization_dataframe.to_csv('./FraudDetection/data/visualization.csv', index=False)
+    features = dataframe.loc[:, dataframe.columns != "PotentialFraud"]
+    labels = dataframe['PotentialFraud']
+    x_train, x_test,y_train, y_test = train_test_split(features,labels,
+                                   random_state=104,
+                                   test_size=0.10,
+                                   shuffle=True)
+    save_test_data(x_test,y_test)
+
+
+if __name__ == "__main__":
+    main()
