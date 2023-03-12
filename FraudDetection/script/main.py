9dfd55ac3a4aeb26ab88a789e1728f26d57ff484
prerit162 <118493234+prerit162@users.noreply.github.com>
2023-03-11 16:27:13 -0800

    Visualization_updated

--- FraudDetection/script/main.py
+++ FraudDetection/script/main.py
@@ -9,13 +9,17 @@ import json
 import io
 from datetime import datetime
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from flask import Flask, request, render_template, session, Response
 
 sys.path.append(os.path.abspath("./FraudDetection/models"))
 
 # pylint: disable=C0413
+# pylint: disable=R1735
 import plotly
-import plotly.express as px
+#import plotly.express as px
+import plotly.graph_objects as go
 from models import loda_anomaly_detection
 from models import ecod_anomaly_detection
 from models import copod_anomaly_detection
@@ -45,9 +49,173 @@ def create_directory_if_not_exists(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def state_wise_visualization(inpatient_final_df,state_mapping):
+    """
+    Computes the state wise distribution of frauds.
+    Args:
+        inpatient_final_df : Dataframe have preprocessed data.
+        state_mapping : Dataframe having state code to state name mapping.
+    """
+    grouped = pd.pivot_table(inpatient_final_df.groupby(
+        ['PotentialFraud','Abbreviation'])['BeneID'].count().reset_index(),
+        values = 'BeneID', index=['Abbreviation'],
+        columns = 'PotentialFraud').reset_index()
+    grouped.reset_index(drop = True)
+    grouped.fillna(0,inplace=True)
+    grouped['Total'] = grouped['No'] + grouped['Yes']
+    grouped['% Frauds'] = grouped['Yes']*100/grouped['Total']
+    grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = False)
+    grouped.loc[grouped['Total'] > 100].head(10)
+    grouped = grouped[['Abbreviation','% Frauds']]
+    grouped.fillna(0,inplace=True)
+    grouped = grouped.merge(state_mapping, on = 'Abbreviation',how = 'inner')
+    grouped['% Frauds'] = grouped['% Frauds'].apply(lambda x: round(x,1))
+    fig = go.Figure(data=go.Choropleth(
+             locations=grouped['Abbreviation'],
+             z=grouped['% Frauds'].astype(float),
+             locationmode='USA-states',
+             colorscale="Viridis_r",
+             autocolorscale=False,
+             text=grouped['State_Name'], # hover text
+             marker_line_color='white', # line markers between states
+        ))
 
+
+    fig.update_layout(
+        title_text = '% of Frauds by State',
+        geo = dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type = 'albers usa'),
+        showlakes=True, # lakes
+        lakecolor='rgb(255, 255, 255)'),
+        )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+def eda(state_mapping):
+    """
+    Creates preprocessed data.
+    Args:
+        state_mapping : Dataframe having state code to state name mapping.
+    """
+    pd.set_option('display.max_columns', None)
+    fraud_providers = pd.read_csv('data/Train-1542865627584.csv')
+    beneficiary = pd.read_csv('data/Train_Beneficiarydata-1542865627584.csv')
+    inpatient = pd.read_csv("data/Train_Inpatientdata-1542865627584.csv")
+    #outpatient = pd.read_csv("data/Train_Outpatientdata-1542865627584.csv")
+    inpatient_intermediate_df = pd.merge(inpatient,beneficiary,
+        on = ['BeneID'], how = 'inner')
+    inpatient_final_df = pd.merge(inpatient_intermediate_df,fraud_providers,
+        on = ['Provider'], how  = 'inner')
+    inpatient_final_df['DischargeDt'] = inpatient_final_df['DischargeDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['AdmissionDt'] = inpatient_final_df['AdmissionDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['ClaimStartDt'] = inpatient_final_df['ClaimStartDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['ClaimEndDt'] = inpatient_final_df['ClaimEndDt'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['DOB'] = inpatient_final_df['DOB'].apply(
+        lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    inpatient_final_df['Day_admitted'] = (inpatient_final_df['DischargeDt']-
+    inpatient_final_df['AdmissionDt'])
+    inpatient_final_df['Day_admitted'] = inpatient_final_df['Day_admitted'].apply(
+        lambda x: x.days)
+    inpatient_final_df['Age'] = inpatient_final_df['DOB'].apply(
+        lambda x: datetime.strptime("2013-03-03", '%Y-%m-%d').year - x.year)
+        #inpatient_final_df['Age']
+    inpatient_final_df = inpatient_final_df.merge(state_mapping, on ='State', how = 'left')
+    inpatient_final_df['Day_admitted'] = (inpatient_final_df['DischargeDt'] -
+         inpatient_final_df['AdmissionDt'])
+    inpatient_final_df['Day_admitted'] = inpatient_final_df['Day_admitted'].apply(
+         lambda x: x.days)
+    inpatient_final_df['Age'] = inpatient_final_df['DOB'].apply(
+         lambda x: datetime.strptime("2013-03-03", '%Y-%m-%d').year - x.year)
+    inpatient_final_df.loc[(inpatient_final_df['Day_admitted'] <= 20),
+         "Days_Admitted_Bucket"] = "0-20 Days"
+    inpatient_final_df.loc[((inpatient_final_df['Day_admitted'] > 20)),
+         "Days_Admitted_Bucket"] = "More than 20 Days"
+    inpatient_final_df.loc[inpatient_final_df['InscClaimAmtReimbursed']<=20000,
+         'InscClaimAmtReimbursed_Bucket'] = '0 - 20000'
+    inpatient_final_df.loc[((inpatient_final_df['InscClaimAmtReimbursed'] > 20000) &
+         (inpatient_final_df['InscClaimAmtReimbursed']<=40000)),
+         'InscClaimAmtReimbursed_Bucket'] = '20000 - 40000'
+    inpatient_final_df.loc[((inpatient_final_df['InscClaimAmtReimbursed'] > 40000)
+         & (inpatient_final_df['InscClaimAmtReimbursed']<=60000)),
+         'InscClaimAmtReimbursed_Bucket'] = '40000 - 60000'
+    inpatient_final_df.loc[inpatient_final_df['InscClaimAmtReimbursed']> 60000,
+         'InscClaimAmtReimbursed_Bucket'] = 'Greater than 60000'
+    return inpatient_final_df
+
+def first_visualization(inpatient_final_df):
+    """
+    Creates a visualization for the trend observed in number of days admitted.
+    Args:
+        inpatient_final_df : Dataframe have preprocessed data.
+    """
+    grouped = pd.pivot_table(inpatient_final_df.groupby(
+         ['PotentialFraud','Days_Admitted_Bucket'])['BeneID'].count().
+         reset_index(), values = 'BeneID',    index=['Days_Admitted_Bucket'],
+         columns = 'PotentialFraud').reset_index()
+    grouped.reset_index(drop = True)
+    grouped.fillna(0,inplace=True)
+    grouped['Total'] = grouped['No'] + grouped['Yes']
+    grouped['% Frauds'] = grouped['Yes']*100/grouped['Total']
+    grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = True)
+    axis = sns.barplot(x = 'Days_Admitted_Bucket',
+            y = '% Frauds',
+            data = grouped)
+    axis.set(xlabel='Number of Days Admitted', ylabel='% Frauds')
+    plt.savefig('FraudDetection/static/images/days_admitted_visualization.jpg')
+
+def third_visualization(inpatient_final_df):
+    """
+    Creates a visualization for the trend observed in Diagnosis Group Code.
+    Args:
+        inpatient_final_df : Dataframe have preprocessed data.
+    """
+    grouped = pd.pivot_table(inpatient_final_df.groupby(
+    ['PotentialFraud','DiagnosisGroupCode'])['BeneID'].count().
+    reset_index(), values = 'BeneID', index=['DiagnosisGroupCode'],
+        columns = 'PotentialFraud').reset_index()
+    grouped.reset_index(drop = True)
+    grouped.fillna(0,inplace=True)
+    grouped['Total'] = grouped['No'] + grouped['Yes']
+    grouped['% Frauds'] = grouped['Yes']*100/grouped['Total']
+    grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = False)
+    top_five_df = grouped.loc[grouped['Total'] > 50].head(5)
+    bottom_five_df = grouped.loc[grouped['Total'] > 50].tail(5)
+    grouped = pd.concat([bottom_five_df,top_five_df])
+    grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = True)
+    axis = sns.barplot(x = 'DiagnosisGroupCode',
+            y = '% Frauds',
+            data = grouped)
+    axis.set(xlabel='Diagnosis Group Code', ylabel='% Frauds')
+    plt.savefig('FraudDetection/static/images/DiagnosisGroupCode.jpg')
+
+
+def fourth_visualization(inpatient_final_df):
+    """
+    Creates a visualization for the trend observed in Claim Amount reimbursed.
+    Args:
+        inpatient_final_df : Dataframe have preprocessed data.
+    """
+    grouped = pd.pivot_table(inpatient_final_df.groupby(
+          ['PotentialFraud','InscClaimAmtReimbursed_Bucket'])['BeneID'].count().
+          reset_index(), values = 'BeneID', index=['InscClaimAmtReimbursed_Bucket'],
+          columns = 'PotentialFraud').reset_index()
+    grouped.reset_index(drop = True)
+    grouped.fillna(0,inplace=True)
+    grouped['Total'] = grouped['No'] + grouped['Yes']
+    grouped['% Frauds'] = grouped['Yes']*100/grouped['Total']
+    grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = True)
+    grouped.loc[grouped['Total'] > 20].head(50)
+    axis = sns.barplot(x = 'InscClaimAmtReimbursed_Bucket',
+            y = '% Frauds',
+            data = grouped)
+    axis.set(xlabel='Insurance Claim Amount Reimbursed', ylabel='% Frauds')
+    plt.savefig('FraudDetection/static/images/Amount_Reimbursed.jpg')
 JSON_FILES = './FraudDetection/script/json'
-
 
 if __name__ == '__main__':
 
@@ -79,61 +247,15 @@ if __name__ == '__main__':
         Returns:
             str: The rendered HTML template.
         """
-        pd.set_option('display.max_columns', None)
-        fraud_providers = pd.read_csv('data/Train-1542865627584.csv')
-        beneficiary = pd.read_csv('data/Train_Beneficiarydata-1542865627584.csv')
-        inpatient = pd.read_csv("data/Train_Inpatientdata-1542865627584.csv")
-        #outpatient = pd.read_csv("data/Train_Outpatientdata-1542865627584.csv")
-        inpatient_intermediate_df = pd.merge(inpatient,beneficiary,
-        on = ['BeneID'], how = 'inner')
-        inpatient_final_df = pd.merge(inpatient_intermediate_df,fraud_providers,
-        on = ['Provider'], how  = 'inner')
-        inpatient_final_df['DischargeDt'] = inpatient_final_df['DischargeDt'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d'))
-        inpatient_final_df['AdmissionDt'] = inpatient_final_df['AdmissionDt'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d'))
-        inpatient_final_df['ClaimStartDt'] = inpatient_final_df['ClaimStartDt'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d'))
-        inpatient_final_df['ClaimEndDt'] = inpatient_final_df['ClaimEndDt'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d'))
-        inpatient_final_df['DOB'] = inpatient_final_df['DOB'].apply(
-        lambda x: datetime.strptime(x, '%Y-%m-%d'))
-        inpatient_final_df['Day_admitted'] = (inpatient_final_df['DischargeDt']-
-        inpatient_final_df['AdmissionDt'])
-        inpatient_final_df['Day_admitted'] = inpatient_final_df['Day_admitted'].apply(
-        lambda x: x.days)
-        inpatient_final_df['Age'] = inpatient_final_df['DOB'].apply(
-        lambda x: datetime.strptime("2013-03-03", '%Y-%m-%d').year - x.year)
-        #inpatient_final_df['Age']
         state_mapping = pd.read_csv("data/State_Mapping.csv")
-        inpatient_final_df = inpatient_final_df.merge(state_mapping, on ='State', how = 'left')
-        grouped = pd.pivot_table(inpatient_final_df.groupby(
-        ['PotentialFraud','Abbreviation'])['BeneID'].count().reset_index(),
-        values = 'BeneID', index=['Abbreviation'], columns = 'PotentialFraud').reset_index()
-        grouped.reset_index(drop = True)
-        grouped.fillna(0,inplace=True)
-        grouped['Total'] = grouped['No'] + grouped['Yes']
-        grouped['% Frauds'] = grouped['Yes']*100/grouped['Total']
-        grouped.sort_values(by = ['% Frauds'],inplace=True, ascending = False)
-        grouped.loc[grouped['Total'] > 100].head(10)
-        grouped = grouped[['Abbreviation','% Frauds']]
-        grouped.fillna(0,inplace=True)
-        fig = px.choropleth(grouped,
-                    locations='Abbreviation',
-                    locationmode="USA-states",
-                    scope="usa",
-                    color='% Frauds',
-                    color_continuous_scale="Viridis_r",
-                    )
-        fig.update_layout(
-        title_text = '% of Frauds by State',
-        title_font_family="Times New Roman",
-        title_font_size = 22,
-        title_font_color="black",
-        title_x=0.45,
-        )
+        inpatient_final_df = eda(state_mapping)
+        first_visualization(inpatient_final_df)
+        third_visualization(inpatient_final_df)
+        fourth_visualization(inpatient_final_df)
+        fig = state_wise_visualization(inpatient_final_df,state_mapping)
         graphjson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
         return render_template('start-page.htm',graphJSON=graphjson)
+
 #        print("Inside home")
 #        return render_template('start-page.htm')
 
