14bac2e0e498a7cd6b1d83fd324ce5494f80c592
Neel Shah <nshah23@uw.edu>
2023-03-14 14:35:00 -0700

    minor change

--- FraudDetection/script/main.py
+++ FraudDetection/script/main.py
@@ -458,8 +458,8 @@ if __name__ == '__main__':
         return send_file(
             filepath,
             mimetype='text/csv',
-            attachment_filename='Frauds.csv',
-            as_attachment=True
+            as_attachment=True,
+            attachment_filename = 'Frauds.csv'
     )
 
     app.run(debug=True)
